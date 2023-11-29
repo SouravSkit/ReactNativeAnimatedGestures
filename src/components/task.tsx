04c5c3570eb007c6fbaf290b4f6113ea510eb779
Sourav <souravhere1999@gmail.com>
2023-11-30 05:19:04 +0530

    Animation1 skia canvas changes along with card swiper

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -1,7 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity, Modal,TextInput,Alert, Dimensions } from 'react-native';
 import Swiper from 'react-native-deck-swiper';
+import { Skia, Canvas, Path, Shader } from '@shopify/react-native-skia';
 
+const source = Skia.RuntimeEffect.Make(`
+  vec3 color1 = vec3(0.635, 0.58, 0.89);
+  vec3 color2 = vec3(0.58, 0.89, 0.882);
+  
+  half4 main(float2 xy) {
+    float t = xy.y / 600.0;
+    float tAdjusted = t * t; 
+    
+    vec3 color = mix(color1, color2, tAdjusted);
+    return vec4(color, 1);
+  }
+`)!;
 
 const data = [
   { id: 1, text: 'Item 1', imageSource: require('../assets/tech2.jpg') },
@@ -37,8 +50,13 @@ const Task = () => {
 
   return (
     <View style={styles.container}>
-<View>
-      <Swiper cards={data}
+        <View style={{ flex: 1 }}>
+        <Canvas style={{ height: Dimensions.get('window').height }}>
+        <Path path="M 0 0 L 1048 0 L 1048 583 L 0 583 Z">
+          <Shader source={source} />
+        </Path>
+      </Canvas>
+        <Swiper cards={data}
       cardIndex={index}
       renderCard={(card)=><Card card={card}
       />}
@@ -58,8 +76,7 @@ const Task = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-     backgroundColor:'#87CEEB'
+  
   },
   
 card: {
@@ -73,11 +90,6 @@ card: {
     alignSelf:'center'
   },
   cardImage:{
-  //   width:160,
-  //   flex:1,
-  //   resizeMode:'contain',
-  //  alignSelf:'center'
-
   width: Dimensions.get('window').width - 130,
   height: Dimensions.get('window').height - 600,
   borderRadius: 8,
@@ -90,89 +102,5 @@ export default Task;
 
 
 
-// import React from 'react';
-// import { View, Text, Image, StyleSheet, Dimensions,ScrollView } from 'react-native';
-// import Animated, { interpolate,useSharedValue } from 'react-native-reanimated';
 
-// const Width = Dimensions.get('window').width;
-// const Height = Dimensions.get('window').height;
-// const Card = ({ item, index, offsetX }) => {
-//   console.log('item,index,offset',item, index, offsetX);
-  
-//   const cardStyle = {
-//     transform: [
-//       {
-//         rotate: interpolate(offsetX, {
-//           inputRange: [
-//             (index - 1) * Width,
-//             index * Width,
-//             (index + 1) * Height,
-//           ],
-//           outputRange: ['-10deg', '0deg', '10deg'],
-//           extrapolate: 'clamp',
-//         }),
-//       },
-//     ],
-//   };
 
-//   return (
-//     <Animated.View style={[styles.card, cardStyle]}>
-//       <Image source={item.imageSource} style={styles.cardImage} />
-//       <Text>{item.text}</Text>
-//     </Animated.View>
-//   );
-// };
-
-// const YourComponent = () => {
-//   const data = [
-//   { id: 1, text: 'Item 1', imageSource: require('../assets/tech2.jpg') },
-//   { id: 2, text: 'Item 2', imageSource: require('../assets/image11.jpg') },
-//   { id: 3, text: 'Item 3', imageSource: require('../assets/image22.jpg') },
-//   { id: 4, text: 'Item 4', imageSource: require('../assets/image33.jpg') },
-//   { id: 6, text: 'Item 6', imageSource: require('../assets/image44.jpg') },
-//   { id: 7, text: 'Item 7', imageSource: require('../assets/image55.jpg') },
-//   { id: 8, text: 'Item 8', imageSource: require('../assets/image66.jpg') },
-// { id: 9, text: 'Item 9', imageSource: require('../assets/image77.jpg') },
-//   { id:10, text: 'Item 10', imageSource: require('../assets/image88.jpg') },
-//   { id:11, text: 'Item 11', imageSource: require('../assets/image99.jpg') },
-//   { id: 12, text: 'Item 12', imageSource: require('../assets/images 1010.jpg') },
-//   { id: 13, text: 'Item 13', imageSource: require('../assets/image1111.jpg') },
-//   { id: 14, text: 'Item 14', imageSource: require('../assets/tech3.jpg') },
-//   { id: 16, text: 'Item 16', imageSource: require('../assets/image44.jpg') },
-//   ];
-
-//   const offsetX = useSharedValue(0); // Assuming offsetX is being updated with the swipe position
-
-//   return (
-//     <ScrollView
-//       horizontal
-//       showsHorizontalScrollIndicator={false}
-//       onScroll={(event) => {
-//         offsetX.value = event.nativeEvent.contentOffset.x;
-//       }}
-//     >
-//       {data.map((item, index) => (
-//         <Card key={item.id} item={item} index={index} offsetX={offsetX} />
-//       ))}
-//     </ScrollView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   card: {
-//     margin: 10,
-//     borderRadius: 8,
-//     borderWidth: 2,
-//     borderColor: '#E8E8E8',
-//     justifyContent: 'center',
-//     backgroundColor: 'white',
-//   },
-//   cardImage: {
-//     width: Dimensions.get('window').width - 40,
-//     height: Dimensions.get('window').height - 200,
-//     borderRadius: 8,
-//     resizeMode: 'cover',
-//   },
-// });
-
-// export default YourComponent;
