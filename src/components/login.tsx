35f15f3ae04c179e8542d0f27768cb2b553c79e1
Sourav <souravhere1999@gmail.com>
2023-12-05 19:08:20 +0530

    Animation1 changes for pagination and blurred effect for the images

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,96 +1,105 @@
-import React, { useState } from 'react';
-import {
-  ScrollView,
-  Text,
-  StyleSheet,
-  TextInput,
-  Pressable,
-  View,
-  Image,
-  ImageBackground
-} from 'react-native';
+import React, { useRef, useState } from 'react';
+import { StyleSheet, Pressable, View, ImageBackground, Text, Image } from 'react-native';
+import { CubeNavigationHorizontal } from 'react-native-3dcube-navigation';
+import { BlurView } from "@react-native-community/blur";
 
-import { CubeNavigationHorizontal } from 'react-native-3dcube-navigation'
-
+const data = [
+  { id: 1, image: require('../assets/image99.jpg') },
+  { id: 2, image: require('../assets/image66.jpg') },
+  { id: 3, image: require('../assets/image11.jpg') },
+];
 
 export default function LoginScreen({ navigation }) {
-  const [email, onChangeEmail] = useState('');
-  const [password, onChangePassword] = useState('');
+  const cubeRef = useRef();
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const onIndexChanged = (index) => {
+    setActiveIndex(index);
+  };
+
+  const commonBlurView = (
+    <BlurView
+      style={styles.absolute}
+      blurType="light"
+      blurAmount={10}
+      reducedTransparencyFallbackColor="white"
+    />
+  );
 
   return (
-      <View style={styles.container} >
-  <CubeNavigationHorizontal ref={view => { this.cube = view; }}>
+    <View style={styles.container}>
+      <CubeNavigationHorizontal ref={cubeRef} onIndexChanged={onIndexChanged}>
+        {data.map((item, index) => (
+          <Pressable key={item.id} onPress={() => navigation.navigate('Task')}>
+            <ImageBackground source={item.image} style={styles.imageContainer}>
+              <View style={styles.paginationContainer}>
+                {data.map((_, i) => (
+                  <View
+                    key={i}
+                    style={[
+                      styles.paginationDot,
+                      { backgroundColor: i === index ? '#fff' : 'rgba(255, 255, 255, 0.5)' },
+                    ]}
+                  />
+                ))}
+              </View>
 
-  <Pressable onPress={() => navigation.navigate('Task')}>
-      <ImageBackground
-        source={require('../assets/image99.jpg')}
-        style={styles.container}
-      >
-      </ImageBackground>
-    </Pressable>
-
-    <Pressable onPress={() => navigation.navigate('Task')}>
-      <ImageBackground
-        source={require('../assets/image66.jpg')}
-        style={styles.container}
-      >
-      </ImageBackground>
-    </Pressable>
-
-    <Pressable onPress={() => navigation.navigate('Task')}>
-      <ImageBackground
-        source={require('../assets/image88.jpg')}
-        style={styles.container}>
-      </ImageBackground>
-    </Pressable>
-
-  </CubeNavigationHorizontal>
-      </View>
-      );
+              <View style={styles.ImageView}>
+                {commonBlurView}
+                <Image
+                  key={'blurryImage'}
+                  source={item.image}
+                  style={styles.absolute1}
+                />
+              </View>
+            </ImageBackground>
+          </Pressable>
+        ))}
+      </CubeNavigationHorizontal>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: '100%',
+    flex: 1,
+  },
+  imageContainer: {
     flex: 1,
     resizeMode: 'cover',
   },
-  headerText: {
-    padding: 40,
-    fontSize: 30,
-    color: '#EDEFEE',
-    textAlign: 'center',
+  paginationContainer: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 20,
+    alignSelf: 'center',
   },
-  regularText: {
-    fontSize: 24,
-    padding: 20,
-    marginVertical: 8,
-    color: '#EDEFEE',
-    textAlign: 'center',
+  paginationDot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 4,
+    color: 'red',
+    marginBottom: 100,
+    zIndex:1
   },
-  father: {
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    fontSize: 16,
-    borderColor: 'EDEFEE',
-    backgroundColor: '#EDEFEE',
+  absolute: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0
   },
-  button: {
-    fontSize: 22,
-    padding: 10,
-    marginVertical: 8,
-    margin: 100,
-    backgroundColor: '#EE9972',
-    borderColor: '#EE9972',
-    borderWidth: 2,
-    borderRadius: 50,
+  absolute1:{
+    position: 'relative',
+    top: '28%',
+    left: '25%',
+    bottom: 0,
+    right: 0,
   },
-  buttonText: {
-    color: 'black',
-    textAlign: 'center',
-    fontSize: 25,
-  },
+  ImageView: {
+    flex: 1,
+  
+  }
 });
 
