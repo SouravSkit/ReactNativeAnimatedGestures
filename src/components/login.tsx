4918d318d1cb775d7325fe8db81da1d73de27e19
Sourav <souravhere1999@gmail.com>
2023-12-24 13:46:35 +0530

    main changes for clendar view

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,7 +1,14 @@
-import React, { useRef, useState } from 'react';
-import { StyleSheet, Pressable, View, ImageBackground, Text, Image } from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Pressable,
+  View,
+  ImageBackground,
+  Text,
+  Image,
+} from 'react-native';
 import { CubeNavigationHorizontal } from 'react-native-3dcube-navigation';
-import { BlurView } from "@react-native-community/blur";
+import { BlurView } from '@react-native-community/blur';
 
 const data = [
   { id: 1, image: require('../assets/image99.jpg') },
@@ -12,8 +19,18 @@ const data = [
 export default function LoginScreen({ navigation }) {
   const cubeRef = useRef();
   const [activeIndex, setActiveIndex] = useState(0);
+  const [pageCount, setPageCount] = useState(6);
+
+  const handlePageCount = () => {
+    setPageCount((prevCount) => prevCount + 1);
+  };
+
+  useEffect(() => {
+    console.log('Page count updated:', pageCount);
+  }, [pageCount]);
 
   const onIndexChanged = (index) => {
+    handlePageCount();
     setActiveIndex(index);
   };
 
@@ -30,15 +47,24 @@ export default function LoginScreen({ navigation }) {
     <View style={styles.container}>
       <CubeNavigationHorizontal ref={cubeRef} onIndexChanged={onIndexChanged}>
         {data.map((item, index) => (
-          <Pressable key={item.id} onPress={() => navigation.navigate('Task')}>
-            <ImageBackground source={item.image} style={styles.imageContainer}>
+          <Pressable
+            key={item.id}
+            onPress={() => navigation.navigate('Task')}
+          >
+            <ImageBackground
+              source={item.image}
+              style={styles.imageContainer}
+            >
               <View style={styles.paginationContainer}>
                 {data.map((_, i) => (
                   <View
                     key={i}
                     style={[
                       styles.paginationDot,
-                      { backgroundColor: i === index ? '#fff' : 'rgba(255, 255, 255, 0.5)' },
+                      {
+                        backgroundColor:
+                          i === index ? '#fff' : 'rgba(255, 255, 255, 0.5)',
+                      },
                     ]}
                   />
                 ))}
@@ -51,6 +77,18 @@ export default function LoginScreen({ navigation }) {
                   source={item.image}
                   style={styles.absolute1}
                 />
+              </View>
+
+              <View style={styles.calendarView}>
+                <View style={styles.calendarContainer}>
+                  <Text style={styles.headerText}>ISSUE N</Text>
+                  <Text style={styles.boldText}>{pageCount}</Text>
+                  <Text style={styles.intro}>WHO IS ERIC LAVOIE?</Text>
+                  <Text style={styles.description}>
+                    Consectetur adipiscing elit. Maecenas various tortor
+                    nibh,sit amet temper
+                  </Text>
+                </View>
               </View>
             </ImageBackground>
           </Pressable>
@@ -81,25 +119,52 @@ const styles = StyleSheet.create({
     marginHorizontal: 4,
     color: 'red',
     marginBottom: 100,
-    zIndex:1
+    zIndex: 1,
   },
   absolute: {
     position: 'absolute',
     top: 0,
     left: 0,
     bottom: 0,
-    right: 0
+    right: 0,
   },
-  absolute1:{
+  absolute1: {
     position: 'relative',
     top: '28%',
     left: '25%',
     bottom: 0,
     right: 0,
   },
+  calendarView: {
+    flex: 0.4,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'white',
+  },
   ImageView: {
-    flex: 1,
-  
-  }
-});
+    flex: 0.6,
+  },
+  headerText: {
+    fontSize: 15,
+    marginTop: 20,
+    color: 'black',
+  },
+  boldText: {
+    fontSize: 70,
+    fontWeight: 'bold',
+    color: 'black',
+    bottom: 10,
+  },
+  intro: {
+    color: '#301934',
+    fontSize: 20,
+    bottom: 10,
+  },
+  description: {
+    fontSize: 13,
+    color: '#301934',
+  },
+  calendarContainer: {
+    marginHorizontal: 35,
+  },});
 
--- src/components/task.tsx
+++ src/components/task.tsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
-import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity, TextInput, Dimensions } from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Dimensions,
+} from 'react-native';
 import Swiper from 'react-native-deck-swiper';
-import { Canvas, Path, Shader, Skia } from '@shopify/react-native-skia';
-import { useNavigation } from '@react-navigation/native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {Canvas, Path, Shader, Skia} from '@shopify/react-native-skia';
+import {useNavigation} from '@react-navigation/native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -29,15 +38,19 @@ const SearchBar = () => {
 
   return (
     <SafeAreaView>
-      <View style={{ justifyContent: 'center', alignSelf: 'center', marginTop: 30 }}>
-        <Image source={require('../assets/vice.png')} style={styles.iconImage} />
+      <View
+        style={{justifyContent: 'center', alignSelf: 'center', marginTop: 30}}>
+        <Image
+          source={require('../assets/vice.png')}
+          style={styles.iconImage}
+        />
       </View>
       <View>
         <TextInput
           style={styles.input}
           placeholder="Search..."
           placeholderTextColor={'black'}
-          onChangeText={(text) => setSearchText(text)}
+          onChangeText={text => setSearchText(text)}
           onSubmitEditing={handleSearch}
         />
       </View>
@@ -46,11 +59,10 @@ const SearchBar = () => {
 };
 
 const data = [
-  { id: 1, text: 'Item 1', imageSource: require('../assets/tech2.jpg') },
-  { id: 2, text: 'Item 2', imageSource: require('../assets/image11.jpg') },
-  { id: 3, text: 'Item 3', imageSource: require('../assets/image22.jpg') },
-  { id: 4, text: 'Item 4', imageSource: require('../assets/image33.jpg') },
-  
+  {id: 1, text: 'Item 1', imageSource: require('../assets/tech2.jpg')},
+  {id: 2, text: 'Item 2', imageSource: require('../assets/image11.jpg')},
+  {id: 3, text: 'Item 3', imageSource: require('../assets/image22.jpg')},
+  {id: 4, text: 'Item 4', imageSource: require('../assets/image33.jpg')},
 ];
 
 const Task = () => {
@@ -65,7 +77,7 @@ const Task = () => {
     navigation.navigate('login');
   };
 
-  const Card = ({ card }) => (
+  const Card = ({card}) => (
     <TouchableOpacity style={styles.card} onPress={onCardPress}>
       <View>
         <Image source={card.imageSource} style={styles.cardImage} />
@@ -81,20 +93,28 @@ const Task = () => {
             <Shader source={source} />
           </Path>
         </Canvas>
-        <View style={{ justifyContent: 'center', alignSelf: 'center', marginTop: 30 }}>
-          <Image source={require('../assets/vice.png')} style={styles.iconImage} />
+        <View
+          style={{
+            justifyContent: 'center',
+            alignSelf: 'center',
+            marginTop: 30,
+          }}>
+          <Image
+            source={require('../assets/vice.png')}
+            style={styles.iconImage}
+          />
         </View>
         <TextInput
           style={styles.input}
           placeholder="Search..."
           placeholderTextColor={'black'}
         />
-        <View style={{ justifyContent: 'center' }}>
+        <View style={{justifyContent: 'center'}}>
           <Swiper
-            style={{ zIndex: 1 }}
+            style={{zIndex: 1}}
             cards={data}
             cardIndex={index}
-            renderCard={(card) => <Card card={card} />}
+            renderCard={card => <Card card={card} />}
             onSwiper={onSwiped}
             stackSize={4}
             stackScale={10}
@@ -103,19 +123,36 @@ const Task = () => {
             disableTopSwipe
             disableBottomSwipe
           />
-          <Text style={{ fontWeight: 'bold', color: 'white', justifyContent: 'center', textAlign: 'center', backgroundColor: '#A294E4' }}>THE ARCHIVE</Text>
+          <Text
+            style={{
+              fontWeight: 'bold',
+              color: 'white',
+              justifyContent: 'center',
+              textAlign: 'center',
+              backgroundColor: '#A294E4',
+            }}>
+            THE ARCHIVE
+          </Text>
         </View>
       </View>
-      <View style={{ flex: 0.15, backgroundColor: '#94e3e0', marginBottom: 2 }}>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 10 }}>
+      <View style={{flex: 0.15, backgroundColor: '#94e3e0', marginBottom: 2}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginHorizontal: 10,
+          }}>
           <Text>All Editions</Text>
-          <Image source={require('../assets/filter2.png')} style={{ width: 20, height: 20 }} />
+          <Image
+            source={require('../assets/filter2.png')}
+            style={{width: 20, height: 20}}
+          />
         </View>
         <FlatList
           data={data}
-          keyExtractor={(item) => item.id.toString()}
+          keyExtractor={item => item.id.toString()}
           horizontal={true}
-          renderItem={({ item }) => (
+          renderItem={({item}) => (
             <Image source={item.imageSource} style={styles.flatListItem} />
           )}
         />
